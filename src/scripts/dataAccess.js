e8644805e7d3db41508b8d9a77279288d0b88e28
ambarnett <andy.barnett09@gmail.com>
2021-06-17 15:51:45 -0500

    finally got choice box for plumber to show on DOM

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -1,8 +1,10 @@
 const applicationState = {
-    requests: []
+    requests: [],
+    plumbers: []
 }
 const API = "http://localhost:8088"
-
+const mainContainer = document.querySelector("#container")
+//#######################################################################################
 export const fetchRequests = () => {
     return fetch(`${API}/requests`)
         .then(response => response.json())
@@ -13,13 +15,9 @@ export const fetchRequests = () => {
             }
         )
 }
-
-export const getRequests = () => {
-    return [...applicationState.requests]
+export const getRequests = () => { 
+    return [...applicationState.requests] 
 }
-
-const mainContainer = document.querySelector("#container")
-
 export const sendRequest = (userServiceRequest) => {
     const fetchOptions = {
         method: "POST",
@@ -28,15 +26,40 @@ export const sendRequest = (userServiceRequest) => {
         },
         body: JSON.stringify(userServiceRequest)
     }
-    
-
     return fetch(`${API}/requests`, fetchOptions)
         .then(response => response.json())
         .then(() => {
             mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
         })
 }
-
+//#######################################################################################
+export const fetchPlumbers = () => {
+    return fetch(`${API}/plumbers`)
+        .then(response => response.json())
+        .then(
+            (servicePlumbers) => {
+                applicationState.plumbers = servicePlumbers
+            }
+        )
+}
+export const getPlumbers = () => { 
+    return [...applicationState.plumbers] 
+}
+export const sendPlumber = (userPlumber) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userPlumber)
+    }
+    return fetch(`${API}/plumbers`, fetchOptions)
+        .then(response => response.json())
+        .then(() => {
+            mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+        })
+}
+//#######################################################################################
 export const deleteRequest = (id) => {
     return fetch(`${API}/requests/${id}`, { method: "DELETE"})
     .then(
